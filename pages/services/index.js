67ae6f87524b2acf40ad0f39e737957106278a49
lososkina <136521052+lososkina@users.noreply.github.com>
2023-09-25 15:40:56 +0300

    Add files via upload

---
+++ pages/services/index.js
@@ -0,0 +1,19 @@
+let visibleCardsAmount = 4;
+const allCards = document.querySelectorAll('.service-card');
+
+function loadMoreCards(event) {
+    if (visibleCardsAmount >= allCards.length) {
+        return event.target.classList.add('d-none');
+    }
+
+    let counter = 0;
+
+    for (let card of allCards) {
+        if (card.classList.contains('d-none')) {
+            card.classList.remove('d-none');
+            visibleCardsAmount++;
+            counter++;
+        }
+        if (counter === 4) break;
+    }
+}
